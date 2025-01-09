2fca6bc6a622149e28dbe02768b794800f19aaf6
balajiki1 <balaji.ki@northeastern.edu>
2025-01-09 17:03:09 -0500

    changes

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'Portfoliohost';
 const assetPrefix = isGithubActions ? `/${repo}/` : '';
 const basePath = isGithubActions ? `/${repo}` : '';
+export default {
 
-module.exports = {
   output: 'export', // Enables static export for Next.js
   assetPrefix: assetPrefix,
   basePath: basePath,
