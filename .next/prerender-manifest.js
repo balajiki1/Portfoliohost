2020f495a084a8aed4539a6d4191b71b9dcecc6b
balajiki1 <balaji.ki@northeastern.edu>
2025-01-09 18:06:24 -0500

    buiild changes

---
+++ .next/prerender-manifest.js
@@ -0,0 +1,1 @@
+self.__PRERENDER_MANIFEST="{\"version\":4,\"routes\":{},\"dynamicRoutes\":{},\"preview\":{\"previewModeId\":\"73cc0cac11fc3072af99e4f42da69b32\",\"previewModeSigningKey\":\"e59e809340ee078665ba0964bc944946825f0f5ce13a752a290d0512051038de\",\"previewModeEncryptionKey\":\"2923805e3cc1d855e4b37072f38b2cd368dbd077a1e5d00441d46567fc915356\"},\"notFoundRoutes\":[]}"
