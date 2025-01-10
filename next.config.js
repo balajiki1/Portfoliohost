10900977d00370b76184a7305f04bc4a0878d067
balajiki1 <balaji.ki@northeastern.edu>
2025-01-10 11:53:32 -0500

    next changes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
 const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'Portfoliohost';
 const assetPrefix = isGithubActions ? `/${repo}/` : '';
-const basePath = isGithubActions ? `/${repo}` : '';
+
 export default {
 
   output: 'export',
